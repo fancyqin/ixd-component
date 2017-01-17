0e241c6332dd272d8c4d0d3bd68871256c583105
fancyqin <fancarter007@163.com>
2017-01-17 19:45:57 +0800

    vue-cli

--- src/entry/home.js
+++
@@ -1,9 +0,0 @@
-var Vue = require('vue');
-var App = require('../views/home.vue');
-
-new Vue({
-    el:'body',
-    components:{
-        app:App
-    }
-})
--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,75 @@
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var path = require('path');
+var path = require('path')
+var webpack = require('webpack')
 
-var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
-
-var plugins = [];
-plugins.push(new ExtractTextPlugin('css/[name].css',{disable: false}));
-if(process.env.NODE_ENV === 'production'){
-    plugins.push([
-        new webpack.DefinePlugin({
-            'process.env':{
-                NODE_ENV:'"production"'
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress:{
-                warnings: false
-            }
-        }),
-        new webpack.optimize.OccurenceOrderPlugin()
-    ]);
-} else{
-    module.exports.devtool = '#source-map'
-}
-plugins.push(new CommonsChunkPlugin({
-    filename:'js/common.js',
-    name:'common'
-}));
 module.exports = {
-    entry:{
-        index:'./src/entry/home.js',
-        test:'./src/entry/test.js'
-    },
-    output: {
-        path: path.join(__dirname,'dist'),
-        filename: 'js/[name].bundle.js'
-    },
-    module:{
-        loaders:[
-            {
-                test: /\.vue$/,
-                loader:'vue'
-            },
-            {
-                test:/\.(png|jpg|gif)$/,
-                loader:'url-loader?limit=8192&name=img/[name].[ext]?[hash]'
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules|vue\/dist/,
-                loader: 'babel'
-            }
-        ]
-    },
-    plugins: plugins,
-    vue:{
-        loaders:{
-            css: ExtractTextPlugin.extract('css'),
-            sass: ExtractTextPlugin.extract('css!sass-loader')
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+            // the "scss" and "sass" values for the lang attribute to the right configs here.
+            // other preprocessors should work out of the box, no loader config like this nessessary.
+            'scss': 'vue-style-loader!css-loader!sass-loader',
+            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+          }
+          // other vue-loader options go here
         }
-    },
-    babel:{
-        presets:['es2015','stage-0'],
-        plugins:['transform-runtime']
-    },
-    watch:true
-};
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.common.js'
+    }
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
+}
 
-
-
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
