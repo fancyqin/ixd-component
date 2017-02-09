33b525866f9a1ad45c6575a947ad14240c04f858
Fan <fancarter007@163.com>
2017-02-09 17:28:50 +0800

    :octocat:

---
+++ src/components/header.vue
@@ -0,0 +1,23 @@
+<style lang="scss">
+    .container{
+        border: 1px solid #0D3349;
+    }
+</style>
+
+<template>
+    <h2>compA</h2>
+    <div class="container">
+        哈哈分发发 <span class="text">{{msg}}</span>
+    </div>
+</template>
+
+
+<script>
+    export default{
+        data(){
+            return{
+                msg:'紫紫紫紫色'
+            }
+        }
+    }
+</script>
